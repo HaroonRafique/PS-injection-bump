af873ae605c1fd6e5371850f4655f860d900cae3
esenes <eugenio.senes@gmail.com>
2018-08-15 14:22:38 +0200

    Fixed (?) RF

--- pyOrbit_no_SC+RF/scripts_bump/_retreive_summary.sh
+++ pyOrbit_no_SC+RF/scripts_bump/_retreive_summary.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-condor_ssh_to_job -ssh scp 210266.0 remote:output/output.mat /afs/cern.ch/work/e/esenes/simulations/bump_up_sextupoles_50k/condor/../scripts_bump/
+condor_ssh_to_job -ssh scp 407656.0 remote:output/output.mat /afs/cern.ch/work/e/esenes/PS-injection-bump/pyOrbit_no_SC+RF/condor/../scripts_bump/
