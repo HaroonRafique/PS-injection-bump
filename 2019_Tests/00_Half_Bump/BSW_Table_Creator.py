3be531c236828298ac17e6018440a5fff008624b
Haroon Rafique <haroon.rafique@protonmail.com>
2019-06-26 12:12:00 +0200

    Simulate bump closure from maximum orbit deviation to zero

---
+++ 2019_Tests/00_Half_Bump/BSW_Table_Creator.py
@@ -0,0 +1,98 @@
+# BSW Table Creator
+# Creates a .TFS table that is read by MAD-X to set magnet strengths over time
+# Haroon Rafique CERN BE-ABP-HSI 07.06.19
+#
+import numpy as np
+import os
+
+from math import log10, floor
+def round_sig(x, sig=5):
+	return round(x, sig-int(floor(log10(abs(x))))-1)
+
+# golden values for the bump
+bsw40   = 0.003498034648
+bsw42   = -0.01422287844
+bsw43   = 0.01290524313
+bsw44   = -0.006001526439
+BSS_max = 2*1.11828e-01 #from twiss
+
+# build the parameter sweep lists
+# ~ nstep = 50.
+nstep = 50.
+
+t = np.arange(0, nstep+1,1)/nstep * np.pi/2
+t = np.append(t, t+np.pi/2)
+
+len_t = len(t)
+
+
+def half_list(a_list):
+    half = len(a_list)//2
+    return a_list[:half], a_list[half:]
+    
+    
+#print t+np.pi/2
+print t
+print len_t
+
+# strengths for the simulation
+#sextupole strength
+BSS_val = BSS_max*np.cos(t)
+
+#quadrupole strength
+bsw40_val = bsw40*np.sin(t)
+bsw42_val = bsw42*np.sin(t)
+bsw43_val = bsw43*np.sin(t)
+bsw44_val = bsw44*np.sin(t)
+
+# We want a table, with K2 columns, one for each sextupole (4)
+# 9 spaces before and between each column
+# with header something like:
+# ~ header = "@ NAME             %08s \"BSWTABLE\"\n@ TYPE             %04s \"USER\"\n@ TITLE            %03s \"BSW\"\n@ ORIGIN           %16s \"5.04.02 Linux 64\"\n@ DATE             %08s \"07/06/19\"\n@ TIME             %08s \"12.09.14\"\n*             BSEXT_T\t\tBSEXT40_K2\t\tBSEXT42_K2\t\tBSEXT43_K2\t\tBSEXT44_K2\n$\t\t%le\t\t%le\t\t%le\t\t%le\t\t%le\n"
+# ~ header = "@ NAME             %08s \"BSWTABLE\"\n@ TYPE             %04s \"USER\"\n@ TITLE            %03s \"BSW\"\n@ ORIGIN           %16s \"5.04.02 Linux 64\"\n@ DATE             %08s \"07/06/19\"\n@ TIME             %08s \"12.09.14\"\n*             BSEXT_T\tBSS\tBSW40\tBSW42\tBSW43\tBSW44\n$\t%le\t%le\t%le\t%le\t%le\t%le\n"
+header = "@ NAME             %08s \"BSWTABLE\"\n@ TYPE             %04s \"USER\"\n@ TITLE            %03s \"BSW\"\n@ ORIGIN           %16s \"5.04.02 Linux 64\"\n@ DATE             %08s \"07/06/19\"\n@ TIME             %08s \"12.09.14\"\n"
+
+
+# MADX
+#-BSStren*(BSW40/BSW42)
+# Equivalent
+#-BSS_val[t]*(bsw40*np.sin(t) / bsw42*np.sin(t))
+
+f = open("BSEXT_Bump.tfs","w")
+
+f.write(header)
+
+s = half_list(t)[1]
+u = half_list(t)[0]
+
+print u
+print s
+
+for i in s:
+	# Sextupole strengths
+	# ~ s40 = -BSS_val[i]*(bsw40*np.sin(i) / bsw42*np.sin(i))
+	# ~ s42 = -BSS_val[i]*(bsw42*np.sin(i) / bsw42*np.sin(i))
+	# ~ s43 = -BSS_val[i]*(bsw43*np.sin(i) / bsw42*np.sin(i))
+	# ~ s44 = -BSS_val[i]*(bsw44*np.sin(i) / bsw42*np.sin(i))	
+	# ~ column = str('\t\t%2.5e\t\t%2.5e\t%2.5e\t%2.5e\t%2.5e\n' % (t[i], s40, s42, s43, s44))
+	
+	# ~ column = str('\t%2.5e\t%2.5e\t%2.5e\t%2.5e\t%2.5e\t%2.5e\n' % (i, bss, s40, s42, s43, s44))
+ 
+	# ~ f.write(column)
+	
+	# Bumper strengths
+	bss = BSS_max*np.cos(i)
+	s40 = bsw40*np.sin(i)
+	s42 = bsw42*np.sin(i)
+	s43 = bsw43*np.sin(i)
+	s44 = bsw44*np.sin(i)
+	
+f.write('*'+' '.join(map(lambda i: i.rjust(18), ['BSEXT_T', 'BSS', 'BSW40_K0', 'BSW42_K0', 'BSW43_K0', 'BSW44_K0'])) + '\n')
+f.write('$'+' '.join(map(lambda i: i.rjust(18), ['%le', '%le', '%le', '%le','%le', '%le'])) + '\n')
+
+# ~ for i in xrange(0,len(t),100):
+for i in xrange(0,len(u)):
+		f.write(' '+' '.join(map(lambda i: ('%1.4e'%i).rjust(18), [u[i], BSS_max*np.cos(s[i]), bsw40*np.sin(s[i]), bsw42*np.sin(s[i]), bsw43*np.sin(s[i]), bsw44*np.sin(s[i])])) + '\n')
+
+
+
