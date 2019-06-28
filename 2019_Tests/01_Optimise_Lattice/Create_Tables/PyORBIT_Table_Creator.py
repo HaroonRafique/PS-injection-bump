7dfe111eba5b19e19f38a0f2099fd0985419f8d1
Haroon Rafique <haroon.rafique@protonmail.com>
2019-06-28 16:59:23 +0200

    PyORBIT_Table_Creator updated

--- 2019_Tests/01_Optimise_Lattice/Create_Tables/PyORBIT_Table_Creator.py
+++ 2019_Tests/01_Optimise_Lattice/Create_Tables/PyORBIT_Table_Creator.py
@@ -26,14 +26,6 @@ def Create_PTC_Table(name, multipole_order, time, normal, skew):
 	
 	return
 
-
-# Table time is in seconds
-# 1 turn = 2.287E-6 seconds
-# We want a full bump of 1E-3 seconds, or a half bump of 5E-4 seconds
-# 2200 turns = 5.0314E-3 s
-# Bump on for 5E-4 seconds
-# zeroes from 5E-4 - 5.0314E-3 s
-
 # Read TFS table
 # We ignore the time column
 def Read_TFS_Return_Data(file_in):
@@ -45,7 +37,7 @@ def Read_TFS_Return_Data(file_in):
 		if ('@' in l) or ('$' in l) or ('*' in l):
 			pass
 		else:
-			data.append(l.split()[1])
+			data.append(float(l.split()[1]))
 
 	return data
 	
@@ -60,7 +52,7 @@ def Create_Timing(ramp_stop_time, simulation_stop_time, data):
 	time = np.linspace(0, ramp_stop_time, d_len)
 	
 	# Calculate interval to complete data until the end of the simulation
-	interval = ramp_stop_time / d_len
+	interval = ramp_stop_time / (d_len-1)
 	
 	# Append zeroes (bump is closed)
 	steps = int((simulation_stop_time - ramp_stop_time)	/ interval) + 5
@@ -75,10 +67,31 @@ def Create_Timing(ramp_stop_time, simulation_stop_time, data):
 	return time, data_2
 
 
-
-
 Create_PTC_Table('TEST', 2, [0,1,2], [11.32161, 12.1321564, 13.555555855555555], [0,0,0])
+
+# Table time is in seconds
+# 1 turn = 2.287E-6 seconds
+# We want a full bump of 1E-3 seconds, or a half bump of 5E-4 seconds
+# 2200 turns = 5.0314E-3 s
+# Bump on for 5E-4 seconds
+# zeroes from 5E-4 - 5.0314E-3 s
 
 B_40 = Read_TFS_Return_Data('BSEXT40.tfs')
 B_40_final = Create_Timing(5E-4, 5.0314E-3, B_40)
 Create_PTC_Table('BSEXT40', 3, B_40_final[0], B_40_final[1], np.zeros(len(B_40_final[0])))
+
+B_42 = Read_TFS_Return_Data('BSEXT42.tfs')
+B_42_final = Create_Timing(5E-4, 5.0314E-3, B_42)
+Create_PTC_Table('BSEXT42', 3, B_42_final[0], B_42_final[1], np.zeros(len(B_42_final[0])))
+
+B_43 = Read_TFS_Return_Data('BSEXT43.tfs')
+B_43_final = Create_Timing(5E-4, 5.0314E-3, B_43)
+Create_PTC_Table('BSEXT43', 3, B_43_final[0], B_43_final[1], np.zeros(len(B_43_final[0])))
+
+B_44 = Read_TFS_Return_Data('BSEXT44.tfs')
+B_44_final = Create_Timing(5E-4, 5.0314E-3, B_44)
+Create_PTC_Table('BSEXT44', 3, B_44_final[0], B_44_final[1], np.zeros(len(B_44_final[0])))
+
+BSM40 = Read_TFS_Return_Data('PI.BSM40.1.tfs')
+BSM40_final = Create_Timing(5E-4, 5.0314E-3, BSM40)
+Create_PTC_Table('BSM40', 2, BSM40_final[0], BSM40_final[1], np.zeros(len(BSM40_final[0])))
