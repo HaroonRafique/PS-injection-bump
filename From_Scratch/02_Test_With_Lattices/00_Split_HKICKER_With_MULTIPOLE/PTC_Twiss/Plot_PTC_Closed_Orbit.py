3e89e2c89ffe31328990a0e5342ada36c2c1f9c8
Haroon Rafique <haroon.rafique@protonmail.com>
2019-08-27 14:03:14 +0200

    From_Scratch: 02_Test_With_Lattices: 00 updated

--- From_Scratch/02_Test_With_Lattices/00_Split_HKICKER_With_MULTIPOLE/PTC_Twiss/Plot_PTC_Closed_Orbit.py
+++ From_Scratch/02_Test_With_Lattices/00_Split_HKICKER_With_MULTIPOLE/PTC_Twiss/Plot_PTC_Closed_Orbit.py
@@ -1,3 +1,8 @@
+# Script to read multiple PTC twiss files and plot the closed orbit
+# Expects a PTC twiss created with the following command:
+# select, flag=ptc_twiss, column=name, s, betx, px, bety, py, disp3, disp3p, disp1, disp1p, x, y;
+# 26.08.19 Haroon Rafique CERN BE-ABP-HSI 
+
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import matplotlib.cm as cm
@@ -19,45 +24,120 @@ plt.rcParams['lines.linewidth'] = 0.5
 extensions = ('.ptc')		# All outputs are .ptc files
 iterators = []				# Integers (turn) used to iterate over files
 
-for subdir, dirs, files in os.walk(rootdir):
+# Search from current directory
+print 'Find PTC Twiss files'
+for subdir, dirs, files in os.walk('.'):
+	# Iterate over all files
     for file in files:
+		# Find files with required extension
 		ext = os.path.splitext(file)[-1].lower()
 		if ext in extensions:
-			print (os.path.join(subdir, file))		# full path to file
-			filename = file.replace('_','.')		# replace _ with a .
-			fileno = int(filename.split('.')[1])	# use turn number as a key
+			# Print full file path
+			# ~ print (os.path.join(subdir, file))		# full path to file
+			fileno = int(file.split('.')[0])	# use turn number as a key
 			iterators.append(fileno)
-			if (fileno <= min_file_no):				# find min turn
-				min_file_no = fileno
-				min_file = file
-			elif (fileno >= max_file_no):			# find max turn
-				max_file_no = fileno
-				max_file = file
+
+data = dict()
+
 
 # iterate over files (turns)
-for turn in iterators:
+print 'Read s, x data from files'
+last_s = 0
+
+for turn in sorted(iterators):
 	
+	s = []
+	x = []
+
 	# Open file
-	
+	infile = str(turn) + '.ptc'
+	fin=open(infile,'r').readlines()[90:]
+
 	# Save s, x
-	
+	for l in fin:
+		if last_s == float(l.split()[1]):
+			pass
+		else:
+			last_s =float(l.split()[1])
+			s.append(float(l.split()[1]))
+			x.append(float(l.split()[-2]))
+
 	# Add to dictionary as dict[turn] = (s, x)
+	data[turn] = [s, x]
+	last_s = 0
 	
 
-# Plot
+# Access turn 0, s column
+# data[0][0]
+# Access turn 25, x column, first value
+# ~ print data[25][1][0]
 
-	fig, ax1 = plt.subplots();
-	plt.title("PTC Injection Closure Tune Swing");
-# Loop over turns
+print 'length of data = ', len(data[25][0])
+print 'max x data = ', max(data[25][1])
+print 'min x of data = ', min(data[25][1])
 
-	# colormap 
-	colors = cm.rainbow(np.linspace(0, 1, len(iterators)))
-	c_it = int(0)
-	
-	
-# For each turn plot s,x in a new colour
 
-	savename = 'PTC_Closed_Orbit.png'
-	plt.savefig(savename, dpi = 800);
+#-----------------------------------------------------------------------
+#------------------------------PLOTTING---------------------------------
+#-----------------------------------------------------------------------
+
+case = '00_Split_HKICKER_with_MULTIPOLE'
+
+print 'Start Plotting'
+
+fig, ax1 = plt.subplots();
+plt.title("PTC Injection Closure Tune Swing");
+
+# colormap 
+colors = cm.rainbow(np.linspace(0, 1, len(iterators)))
+c_it = int(0)
+
+ax1.set_xlabel("S [m]");
+ax1.set_ylabel("x [m]");
+
+for turn in sorted(iterators):
+	print 'Plotting turn ', turn
+	plt.plot(data[turn][0], data[turn][1], color=colors[c_it])
+
+	# For each turn plot s,x in a new colour
+	c_it += 1
+
+ax1.grid()
+
+savename = 'PTC_Closed_Orbit' + case + '.png'
+plt.savefig(savename, dpi = 800);
+
+print 'Plot 1 done'
+
+
+fig, ax1 = plt.subplots();
+plt.title("PTC Injection Closure Tune Swing");
+
+# colormap 
+colors = cm.rainbow(np.linspace(0, 1, len(iterators)))
+c_it = int(0)
+
+ax1.set_xlim(470.0, 510.0)
+
+ax1.set_xlabel("S [m]");
+ax1.set_ylabel("x [m]");
+
+test  = [1]
+
+# ~ plt.plot(data[1][0], data[1][1], color=colors[c_it])
+
+for turn in sorted(iterators):
+	print 'Plotting turn ', turn
+	plt.plot(data[turn][0], data[turn][1], color=colors[c_it])
+
+	# For each turn plot s,x in a new colour
+	c_it += 1
+
+ax1.grid()
+
+savename = 'PTC_Closed_Orbit' + case + '_zoom.png'
+plt.savefig(savename, dpi = 800);
+
+print 'Plot 2 done'
 
 
