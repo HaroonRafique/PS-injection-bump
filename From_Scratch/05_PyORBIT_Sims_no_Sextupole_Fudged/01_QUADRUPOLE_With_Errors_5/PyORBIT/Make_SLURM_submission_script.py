108367de2a9f9ed37c3a1d39c22c788af641faf0
Haroon Rafique <haroon.rafique@cern.ch>
2019-09-02 15:05:25 +0200

    From_Scratch: 05 sims updated

--- From_Scratch/05_PyORBIT_Sims_no_Sextupole_Fudged/01_QUADRUPOLE_With_Errors_5/PyORBIT/Make_SLURM_submission_script.py
+++ From_Scratch/05_PyORBIT_Sims_no_Sextupole_Fudged/01_QUADRUPOLE_With_Errors_5/PyORBIT/Make_SLURM_submission_script.py
@@ -23,7 +23,7 @@ queue = 'inf-short'
 
 n_nodes = 1
 
-jobname = '04_02'
+jobname = '05_01'
 
 path_to_simulation = os.path.dirname(os.path.realpath(__file__)) # This directory
 
--- From_Scratch/05_PyORBIT_Sims_no_Sextupole_Fudged/03_SBEND_With_Errors_0p2/PyORBIT/Make_SLURM_submission_script.py
+++ From_Scratch/05_PyORBIT_Sims_no_Sextupole_Fudged/03_SBEND_With_Errors_0p2/PyORBIT/Make_SLURM_submission_script.py
@@ -23,7 +23,7 @@ queue = 'inf-short'
 
 n_nodes = 1
 
-jobname = '04_03'
+jobname = '05_03'
 
 path_to_simulation = os.path.dirname(os.path.realpath(__file__)) # This directory
 
